49fe36558e01ae898e2a76abdb0dee39322bc026
maldit0 <luciano.montanares@gmail.com>
2021-07-12 00:08:26 -0400

    fix delivery model

--- delivery/models.py
+++ delivery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+
 from auth_app.models import User
 from package.models import Package
 
@@ -8,17 +9,17 @@ from package.models import Package
 
 class Delivery(models.Model):
     id_user_A = models.ForeignKey(User, on_delete=models.PROTECT, related_name='sender')
-    id_user_B = models.ForeignKey(User, on_delete=models.PROTECT, related_name='transport', blank=True)
+    id_user_B = models.ForeignKey(User, on_delete=models.PROTECT, related_name='transport', null=True, blank=True)
     id_package_1 = models.ForeignKey(Package, on_delete=models.PROTECT, related_name='package_1')
     phone_receiver = models.CharField(max_length=20)
     name_receiver = models.CharField(max_length=20)
     metro_init = models.CharField(max_length=20)
     metro_final = models.CharField(max_length=20)
-    #id_package_2 = models.ForeignKey(User, on_delete=models.PROTECT, related_name='package_2', default=None)
-    #lat_initial = models.FloatField(null=True, max_length=50)
-    #lng_initial = models.FloatField(null=True, max_length=50)
-    #lat_final = models.FloatField(null=True, max_length=50)
-    #lng_final = models.FloatField(null=True, max_length=50)
+    # id_package_2 = models.ForeignKey(User, on_delete=models.PROTECT, related_name='package_2', default=None)
+    # lat_initial = models.FloatField(null=True, max_length=50)
+    # lng_initial = models.FloatField(null=True, max_length=50)
+    # lat_final = models.FloatField(null=True, max_length=50)
+    # lng_final = models.FloatField(null=True, max_length=50)
     date_initial = models.DateTimeField(default=timezone.now)
     date_final = models.DateTimeField(null=True)
     distance = models.FloatField(null=True, max_length=50)
