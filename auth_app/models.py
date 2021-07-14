5af4c9a4c38d7db6903aa61147a6a7121c6ee957
maldit0 <52437683+maldit0@users.noreply.github.com>
2021-07-14 18:03:49 -0400

    fix delivery model

--- auth_app/models.py
+++ auth_app/models.py
@@ -15,7 +15,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
 
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20, unique=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField(_('email address'), unique=True)
